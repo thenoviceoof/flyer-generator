5f6be22244353e9b232b17fec78b9c0d86793ac0
Zhehao Mao <zhehao.mao@gmail.com>
2011-08-21 12:10:05 -0400

    make code a bit cleaner

--- flyer.py
+++ flyer.py
@@ -8,7 +8,7 @@ import cherrypy
 
 # templating engine
 import jinja2
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 # utility modules
 import os
@@ -16,31 +16,26 @@ import subprocess
 
 # cherrypy class
 class Flyer():
-    _cp_config = {
+    def __init__(self):
+        self.env = Environment(loader = FileSystemLoader('templates'))
+	
+	_cp_config = {
         'tools.staticdir.on': True,
         'tools.staticdir.dir' : os.getcwd()
         }
     def index(self):
         # grab the front page template from file
         # replace with FileLoader
-        f = open("static/front.html")
-        s = f.read()
-        temp = Template(s)
+        temp = self.env.get_template('front.html')
         return temp.render()
     def render(self, title="", subtitle="", content=""):
         # convert the template
-        f = open("static/adi-1.html")
-        s = f.read()
-        f.close()
-        temp = Template(s)
+        temp = self.env.get_template('adi-1.html')
         # write it out to a temporary file
         return temp.render(title=title, subtitle=subtitle, content=content)
     def flyer(self, title="", subtitle="", content=""):
         # convert the template
-        f = open("static/adi-1.html")
-        s = f.read()
-        f.close()
-        temp = Template(s)
+        temp = self.env.get_template('adi-1.html')
         # write it out to a temporary file
         f = open("tmp/adi.html","w")
         f.write(temp.render(title=title, subtitle=subtitle, content=content))
