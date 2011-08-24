c563963825d5cdb1f6588635f8721bbbbfc75867
Nathan Hwang <beta.entity.k@gmail.com>
2011-08-24 00:04:40 -0700

    Pushed in a lolhawk template, to jumpstart the live view

--- flyer.py
+++ flyer.py
@@ -16,18 +16,26 @@ import subprocess
 
 # cherrypy class
 class Flyer():
-    def __init__(self):
-        self.env = Environment(loader = FileSystemLoader('templates'))
-	
-	_cp_config = {
+    _cp_config = {
         'tools.staticdir.on': True,
         'tools.staticdir.dir' : os.getcwd()
         }
+
+    def __init__(self):
+        self.env = Environment(loader = FileSystemLoader('templates'))
+	
     @cherrypy.expose
     def index(self):
         # grab the front page template from file
         # replace with FileLoader
         temp = self.env.get_template('front.html')
+        return temp.render()
+
+    # experimental: shoving the lolhawk stuff into this
+    @cherrypy.expose
+    def lolhawk(self):
+        # grab the front page template from file
+        temp = self.env.get_template('front-2.html')
         return temp.render()
     
     @cherrypy.expose
@@ -52,4 +60,5 @@ class Flyer():
 
 if __name__ == "__main__":
     # start the engine on 8080
+    # if you don't have a site.conf, just do a `touch site.conf`
     cherrypy.quickstart(Flyer(), '/', 'site.conf')
---
+++ static/js/front-2.js
@@ -0,0 +1,98 @@
+var fonts = [
+    ["Serif",
+     ["Droid Serif",
+      "Cousine",
+      "OFL Sorts Mill Goudy TT",
+      "Vollkorn",
+      "Crimson Text",
+      "Arvo"]],
+    ["Sans Serif",
+     ["Droid Sans",
+      "Nobile",
+      "Molengo",
+      "PT Sans",
+      "Inconsolata",
+      "Droid Sans Mono",
+      "Josefin Slab"]],
+    ["Other",
+     ["Tangerine",
+      "Philosopher",
+      "Lobster",
+      "Reenie Beanie"]]];
+
+// loads Fonts dynamically
+// http://www.hunlock.com/blogs/Howto_Dynamically_Insert_Javascript_And_CSS
+function loadFont(fontName) {
+    var head = document.getElementsByTagName("head")[0];
+    var style = document.createElement("link");
+    style.type="text/css";
+    style.rel ="stylesheet";
+    style.href="http://fonts.googleapis.com/css?family="+fontName.replace(" ","+");
+    head.appendChild(style);
+}
+
+$(function() {
+    $("#editor").dialog({width: "auto", title: "Flyer Generator r004"});
+    $("#date").datepicker({ showButtonPanel:true, numberOfMonths: 2 });
+    // otherwise, only changes on blur/focus
+    $("input, textarea").keyup(function() { $(this).change() });
+    $("input, textarea").change(function() {
+        var data = $(this).val();
+        var d = data.replace(/\n/g,"<br/>");
+        $("#"+$(this).attr("id")+"_body").html(d);
+    });
+    var debug = false;
+    $("#debug").click(function(event) {
+        if(debug) {
+            $("body").css("border","");
+            debug = false;
+        } else {
+            $("body").css("border","1px black solid");
+            debug = true;
+        }
+    });
+    $("#format").change(function() {
+        $( "#date" ).datepicker( "option", "dateFormat", $(this).val() );
+    });
+    // set up way to reopen dialog in a non-intrusive way
+    $("body").dblclick(function() {
+        $("#editor").dialog("open");
+        return false;
+    });
+
+    // load fonts
+    for(var fam in fonts) {
+        for(var f in fonts[fam][1]) {
+            loadFont(fonts[fam][1][f]);
+        }
+    }
+    // add fonts to the font chooser
+    for(var fam in fonts) {
+        var family = fonts[fam][0];
+        for(var f in fonts[fam][1]) {
+            var font = fonts[fam][1][f];
+            var header = $("<h3></h3>").html(family+" - "+font);
+            header.css("font-family",font);
+            $("#fontChooser").append(header);
+            var example = $("<div></div>").html("abcdefghijklmnopqrstuvwxyz0123456789");
+            example.css("font-family",font);
+            $("#fontChooser").append(example);
+            header.data('font',font);
+            example.data('font',font);
+        }
+    }
+
+    $("#styleEditor").dialog({title:"Style Editor",autoOpen:false,model:true,width:'auto'});
+    $("#styler").click(function(){
+        $("#styleEditor").dialog("open");
+    });
+    $("#fontChooser").accordion({autoHeight:false});
+    $("#fontFamily").click(function(){
+        $("#fontChooserContainer").toggle();
+    });
+    $("#fontChooser > h3, #fontChooser > div").dblclick(function(){
+        $("#page").css("font-family",$(this).data('font'));
+        $("#fontChooserContainer").hide();
+        return false;
+    });
+});
